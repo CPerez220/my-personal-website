358642bc5cfdad323e19eb0e1d24894c4e945d66
Carlos Perez <cmperez0730@gmail.com>
2024-05-08 22:07:24 -0700

    Navbar Made

--- Carlos-Perez/src/components/Header.jsx
+++ Carlos-Perez/src/components/Header.jsx
@@ -1,7 +1,27 @@
+import Nav from 'react-bootstrap/Nav';
+
 const Header = () => {
   return (
-    <h1>Header</h1>
-  )
+    <>
+      <Nav className="justify-content-center" activeKey="/">
+        <Nav.Item>
+          <Nav.Link href="/">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Projects</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Videos</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-3">About</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-4">Contact</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
+  );
 }
 
 export default Header
